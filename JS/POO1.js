86b93371cc26604c5a3276aa2ff3a6489e10867b
JLGM <juss.jlgm@gmail.com>
2022-07-30 13:44:59 -0300

    se resolvió ejercicio 1

--- JS/POO1.js
+++ JS/POO1.js
@@ -2,19 +2,36 @@
 //  Crea los métodos necesarios para permitir encender y apagar el auto.
 
 let auto = {
-color: ['Rojo', 'Negro', 'Azul', 'Plateado'],
-marca: 'Citroen',
-modelo: 'Cactus Shine THP 1.6',
-encender: ()=> {document.write(`<p>El auto está encendido....</p>`)},
-apagar : ()=> {document.write(`<p>El auto esta apagado</p>`)}
+  color: ["Rojo", "Negro", "Azul", "Plateado"],
+  marca: "Citroen",
+  modelo: "Cactus Shine THP 1.6",
+  estado: "Comprobando el estado.....",
+  encender (on) {
+    if (ignicion == 0){
+        
+        return document.write(`<p>El auto está encendido....</p>`)
+    } 
+    },
+  apagar (off) {
+if (ignicion == 1)
 
-}
-console.log(auto);
-document.write(`El color elegido es: ${auto.color[3]}`);
+    return document.write(`<p>El auto esta apagado</p>`)
+    },
+};
+
+color = prompt(
+  `Ingrese un color: Sus opciones son Rojo = 0, Negro =1, Azul =2 o Plateado = 3`
+);
+document.write(`El color elegido es: ${auto.color[color]}`);
 document.write(`<p> La marca es ${auto.marca}`);
 document.write(`<p> Su modelo es: ${auto.modelo}`);
+let ignicion = prompt("Presione 0 si quiere encender y 1 para apagar: ");
+document.write(`<p>${auto.estado}</p>`);
 auto.encender();
-document.write('Todo funciona correctamente, ahora sí...');
+
 auto.apagar();
+
+
+
 
 
